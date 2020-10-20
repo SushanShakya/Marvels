c57c1fa7100f65d3a4ef0055021557f4a9c24d66
Sushan <sushaanshakya88@gmail.com>
2020-10-20 10:24:30 +0545

    Marvel_character: Creates test and data class for marvel_character

---
+++ lib/models/marvel_character.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/foundation.dart';
+
+class MarvelCharacter {
+  int id;
+  String name;
+  String description;
+  String image;
+  List<Url> urls;
+
+  MarvelCharacter(
+      {this.id, this.name, this.description, this.image, this.urls});
+
+  factory MarvelCharacter.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return MarvelCharacter(
+        id: map['id'],
+        name: map['name'],
+        description: map['description'],
+        image: map['thumbnail']['path'] + "." + map['thumbnail']['extension'],
+        urls: List<Url>.from(map["urls"].map((x) => Url.fromJson(x))));
+  }
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is MarvelCharacter &&
+        o.id == id &&
+        o.name == name &&
+        o.description == description &&
+        o.image == image &&
+        listEquals(o.urls, urls);
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        description.hashCode ^
+        image.hashCode ^
+        urls.hashCode;
+  }
+
+  @override
+  String toString() {
+    return 'MarvelCharacter(id: $id, name: $name, description: $description, image: $image, urls: $urls)';
+  }
+}
+
+class Url {
+  String type;
+  String url;
+  Url({
+    this.type,
+    this.url,
+  });
+
+  factory Url.fromJson(Map<String, dynamic> json) => Url(
+        type: json["type"],
+        url: json["url"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "type": type,
+        "url": url,
+      };
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is Url && o.type == type && o.url == url;
+  }
+
+  @override
+  int get hashCode => type.hashCode ^ url.hashCode;
+
+  @override
+  String toString() => 'Url(type: $type, url: $url)';
+}
---
+++ test/marvel_character_test.dart
@@ -0,0 +1,141 @@
+import 'package:marvels_app/models/marvel_character.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test("Characters with same attributes are same !", () {
+    MarvelCharacter character = MarvelCharacter(
+      id: 20,
+      name: "Captain America",
+      image: "From internet",
+      description: "",
+    );
+
+    MarvelCharacter character1 = MarvelCharacter(
+      id: 20,
+      name: "Captain America",
+      image: "From internet",
+      description: "",
+    );
+
+    expect(true, character == character1);
+  });
+  test("Characters with different attributes are different !", () {
+    MarvelCharacter character = MarvelCharacter(
+      id: 20,
+      name: "Captain",
+      image: "From internet",
+      description: "",
+    );
+
+    MarvelCharacter character1 = MarvelCharacter(
+      id: 20,
+      name: "Captain America",
+      image: "From internet",
+      description: "",
+    );
+
+    expect(false, character == character1);
+  });
+
+  test("Characters can be created From Json Thrown by Api", () {
+    final map = {
+      "id": 1011334,
+      "name": "3-D Man",
+      "description": "",
+      "modified": "2014-04-29T14:18:17-0400",
+      "thumbnail": {
+        "path": "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784",
+        "extension": "jpg"
+      },
+      "resourceURI": "http://gateway.marvel.com/v1/public/characters/1011334",
+      "urls": [
+        {
+          "type": "detail",
+          "url":
+              "http://marvel.com/characters/74/3-d_man?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d"
+        },
+        {
+          "type": "wiki",
+          "url":
+              "http://marvel.com/universe/3-D_Man_(Chandler)?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d"
+        },
+        {
+          "type": "comiclink",
+          "url":
+              "http://marvel.com/comics/characters/1011334/3-d_man?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d"
+        }
+      ]
+    };
+    MarvelCharacter character = MarvelCharacter.fromMap(map);
+
+    MarvelCharacter character1 = MarvelCharacter(
+        id: 1011334,
+        name: "3-D Man",
+        description: "",
+        image: "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg",
+        urls: <Url>[
+          Url(
+              type: "detail",
+              url:
+                  "http://marvel.com/characters/74/3-d_man?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d"),
+          Url(
+              type: "wiki",
+              url:
+                  "http://marvel.com/universe/3-D_Man_(Chandler)?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d"),
+          Url(
+              type: "comiclink",
+              url:
+                  "http://marvel.com/comics/characters/1011334/3-d_man?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d")
+        ]);
+
+    expect(character, character1);
+  });
+
+  test("List of marvel characters with same length are equal", () {
+    List<MarvelCharacter> characters = [
+      MarvelCharacter(
+          id: 1011334,
+          name: "3-D Man",
+          description: "",
+          image:
+              "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg",
+          urls: <Url>[
+            Url(
+                type: "detail",
+                url:
+                    "http://marvel.com/characters/74/3-d_man?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d"),
+            Url(
+                type: "wiki",
+                url:
+                    "http://marvel.com/universe/3-D_Man_(Chandler)?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d"),
+            Url(
+                type: "comiclink",
+                url:
+                    "http://marvel.com/comics/characters/1011334/3-d_man?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d")
+          ])
+    ];
+
+    expect(characters, [
+      MarvelCharacter(
+          id: 1011334,
+          name: "3-D Man",
+          description: "",
+          image:
+              "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg",
+          urls: <Url>[
+            Url(
+                type: "detail",
+                url:
+                    "http://marvel.com/characters/74/3-d_man?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d"),
+            Url(
+                type: "wiki",
+                url:
+                    "http://marvel.com/universe/3-D_Man_(Chandler)?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d"),
+            Url(
+                type: "comiclink",
+                url:
+                    "http://marvel.com/comics/characters/1011334/3-d_man?utm_campaign=apiRef&utm_source=6a9451a902cf5312d49d9b2b6426009d")
+          ])
+    ]);
+  });
+}
