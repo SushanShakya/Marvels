45d40b5e87c29e366d0b49f27391a02b38ae4da0
SushanShakya <sushaanshakya88@gmail.com>
2020-11-05 13:23:34 +0545

    Search Delegate: Fixes issue of Search and Color of query

--- lib/search/marvel_search_delegate.dart
+++ lib/search/marvel_search_delegate.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marvels_app/cubits/search_characters/searchcharacters_cubit.dart';
-import 'package:marvels_app/repository/marvels_api_repository.dart';
+
 import 'package:marvels_app/screens/widgets/characters_grid.dart';
-import 'package:marvels_app/services/network/marvel_network_service.dart';
 
 class MarvelSearchDelegate extends SearchDelegate {
-  final cubit =
-      SearchCharactersCubit(MarvelsApiRepository(MarvelNetworkService()));
+  final SearchCharactersCubit cubit;
+
+  MarvelSearchDelegate(this.cubit);
 
   @override
   ThemeData appBarTheme(BuildContext context) {
     final ThemeData theme = Theme.of(context);
 
-    return theme.copyWith(appBarTheme: AppBarTheme());
+    return theme.copyWith(
+        textTheme: TextTheme(
+            headline6: TextStyle(
+                color: Colors.white,
+                fontFamily: "Roboto Condensed",
+                fontSize: 18.0)));
+  }
+
+  @override
+  void showResults(BuildContext context) {
+    cubit.search(query);
+    super.showResults(context);
   }
 
   @override
@@ -30,9 +41,17 @@ class MarvelSearchDelegate extends SearchDelegate {
           onPressed: () {
             cubit.search(query);
             showResults(context);
+          }),
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = '';
           })
     ];
   }
+
+  @override
+  void showSuggestions(BuildContext context) {}
 
   @override
   Widget buildLeading(BuildContext context) {
@@ -49,19 +68,23 @@ class MarvelSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return BlocProvider(
-      create: (context) => cubit,
-      child: BlocBuilder<SearchCharactersCubit, SearchCharactersState>(
-        builder: (context, state) {
-          if (state is SearchError) return _buildError(state.message);
-          if (state is SearchSuccess)
-            return CharactersGrid(
-              characters: state.characters,
-            );
+    return BlocConsumer<SearchCharactersCubit, SearchCharactersState>(
+      cubit: cubit,
+      listener: (context, state) {
+        if (state is SearchLoading) print("Loading");
+        if (state is SearchError) print(state.message);
+        if (state is SearchSuccess) print(state.characters);
+      },
+      builder: (context, state) {
+        if (state is SearchLoading) return _buildLoading();
+        if (state is SearchError) return _buildError(state.message);
+        if (state is SearchSuccess)
+          return CharactersGrid(
+            characters: state.characters,
+          );
 
-          return _buildLoading();
-        },
-      ),
+        return Container();
+      },
     );
   }
 
